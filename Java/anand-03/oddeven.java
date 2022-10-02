677dcd4f294dc799880dd942fe042ed4699a1d3d
Anand <73114463+Anand4756@users.noreply.github.com>
2022-10-02 09:35:42 +0530

    added solution of issue #3 even odd

---
+++ Java/anand-03/oddeven.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+
+public class oddeven
+{
+	public static void main(String[] args) {
+		//main code 
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		if(n%2==0){
+			System.out.println("EVEN");
+		}else{
+			System.out.println("ODD");
+		}
+	   
+	}
+}
